f4edb6336a5b55fc4ec06319c2c2c40c00d49e0f
bharatvitkar <79301656+bharatvitkar@users.noreply.github.com>
2022-09-03 09:53:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(290,290);
+    canvas.center();
+
+}
+
+function draw()
+{
+
+}
+
+function take_snapshot()
+{
+    save('myfilterimage.png');
+}
